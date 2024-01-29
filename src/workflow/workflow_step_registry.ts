bfdaa0fd35e654feebd54ee467c9825c1e61b196
Giselle van Dongen <giselle.vd@gmail.com>
2024-01-29 14:54:52 +0100

    Rebase on main

--- src/workflow/workflow_step_registry.ts
+++ src/workflow/workflow_step_registry.ts
@@ -5,13 +5,13 @@ import * as rotateImgService from "../transformers/rotate_img_service";
 import * as blurImgService from "../transformers/blur_img_service";
 import * as stableDiffusionTransformer from "../stable-diffusion/stable_diffusion_transformer";
 
-export const workflowStepRegistry = new Map<string, WorkflowStepProcessor>([
+const workflowStepRegistry = new Map<string, WorkflowStepProcessor>([
     //sources
-    ["puppeteer-service", {processorType: ProcessorType.SOURCE, api: puppeteer.service, router: puppeteer.router} as WorkflowStepProcessor],
-    ["stable-diffusion-generator", {processorType: ProcessorType.SOURCE, api: stableDiffusionGenerator.service, router: stableDiffusionGenerator.router} as WorkflowStepProcessor],
+    ["puppeteer-service", {processorType: ProcessorType.SOURCE, api: puppeteer.service} as WorkflowStepProcessor],
+    ["stable-diffusion-generator", {processorType: ProcessorType.SOURCE, api: stableDiffusionGenerator.service} as WorkflowStepProcessor],
 
     //transformers
-    ["rotate-img-service", {processorType: ProcessorType.TRANSFORMER, api: rotateImgService.service, router: rotateImgService.router} as WorkflowStepProcessor],
-    ["blur-img-service", {processorType: ProcessorType.TRANSFORMER, api: blurImgService.service, router: blurImgService.router} as WorkflowStepProcessor],
-    ["stable-diffusion-transformer", {processorType: ProcessorType.TRANSFORMER, api: stableDiffusionTransformer.service, router: stableDiffusionTransformer.router} as WorkflowStepProcessor]
+    ["rotate-img-service", {processorType: ProcessorType.TRANSFORMER, api: rotateImgService.service} as WorkflowStepProcessor],
+    ["blur-img-service", {processorType: ProcessorType.TRANSFORMER, api: blurImgService.service} as WorkflowStepProcessor],
+    ["stable-diffusion-transformer", {processorType: ProcessorType.TRANSFORMER, api: stableDiffusionTransformer.service} as WorkflowStepProcessor]
 ]);
