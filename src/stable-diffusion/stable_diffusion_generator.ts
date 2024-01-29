29aa0db64da307209edb29cf9ff294ad36f05a10
Till Rohrmann <till@restate.dev>
2024-01-29 17:59:16 +0100

    Add explaining comments for awakeables

--- src/stable-diffusion/stable_diffusion_generator.ts
+++ src/stable-diffusion/stable_diffusion_generator.ts
@@ -22,9 +22,11 @@ async function generateStableDiffusionImg(ctx: restate.RpcContext, imgOutputPath
     const awakeable = ctx.awakeable<string>();
 
     await ctx.sideEffect(async () => {
+        // invoke the stable diffusion service with our awakeable as callback
         await axios.post("http://localhost:5050/generate", { params: params, callback: awakeable.id });
     });
 
+    // wait for the callback from the stable diffusion service containing the generated image
     const generatedImg = await awakeable.promise;
 
     const decodedImage: Buffer = Buffer.from(generatedImg, "base64");
--- src/stable-diffusion/stable_diffusion_transformer.ts
+++ src/stable-diffusion/stable_diffusion_transformer.ts
@@ -26,9 +26,11 @@ async function transformImgWithStableDiffusion(ctx: restate.RpcContext, imgOutpu
     const awakeable = ctx.awakeable<string>();
 
     await ctx.sideEffect(async () => {
+        // invoke the stable diffusion service with our awakeable as callback
         await axios.post("http://localhost:5050/generate", { params: params, callback: awakeable.id });
     });
 
+    // wait for the callback from the stable diffusion service containing the generated image
     const generatedImg = await awakeable.promise;
 
     const decodedImage: Buffer = Buffer.from(generatedImg, "base64");
